1ae1dbd1b6f242659acad1c448afde407d6e5158
randy hac <randydhacks@gmail.com>
2023-02-23 21:06:19 -0800

    sdsd

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,54 +7,76 @@ let board = new Board(); // creates a new game board
 console.log(board.grid);
 
 const createGridContainer = () => {
+  const gridContainer = document.createElement("div");
+  gridContainer.id = "container";
+  document.body.appendChild(gridContainer);
 
-    const gridContainer = document.createElement('div')
-    gridContainer.id = 'container'
-    document.body.appendChild(gridContainer)
+  const createSquares = () => {
+    for (let i = 0; i < board.grid.length; i++) {
+      let row = board.grid[i];
+      let htmlRow = document.createElement("div");
+      htmlRow.className = "row";
+      htmlRow.id = i;
+      gridContainer.appendChild(htmlRow);
 
-    const createSquares = () => {
+      for (let j = 0; j < row.length; j++) {
+        const square = document.createElement("div");
+        htmlRow.appendChild(square);
+        square.dataset.row = i;
+        square.dataset.col = j;
+        square.className = "square";
 
-        for (let i = 0; i < board.grid.length; i++) {
-            let row = board.grid[i]
-            let htmlRow = document.createElement('div')
-            htmlRow.className = 'row'
-            htmlRow.id = i
-            gridContainer.appendChild(htmlRow)
+        square.addEventListener("click", (e) => {
+            board.makeHit(i,j)
+            clickSquare(e)
+            console.log(board.numRemaining)
 
-            for (let j = 0; j < row.length; j++) {
-                const square = document.createElement('div')
-                htmlRow.appendChild(square)
-                square.dataset.row = i
-                square.dataset.col = j
-                square.className = 'square'
-
-                square.addEventListener('click', (e) => clickSquare(e))
-
+            if (board.isGameOver()) {
+                alert('Game Over')
             }
-        }
+        });
+      }
     }
-    createSquares()
-}
+  };
+  createSquares();
+};
 
 const clickSquare = (event) => {
+  const row = event.target.dataset.row;
+  const col = event.target.dataset.col;
 
-    const row = event.target.dataset.row
-    const col = event.target.dataset.col
+  const checkSquare = board.grid[row][col];
 
-    const checkSquare = board.grid[row][col]
+  if (checkSquare) {
+    event.target.style.backgroundColor = "green";
+    event.target.innerText = checkSquare;
+  } else {
+    event.target.style.backgroundColor = "red";
+  }
+};
 
-    if (checkSquare) {
-        event.target.style.backgroundColor = 'green'
-        event.target.innerText = checkSquare
+const createResetButton = () => {
+    const resetButton = document.createElement('button')
+    resetButton.id = 'reset'
+    document.body.appendChild(resetButton)
+    resetButton.innerText = 'Reset Game'
 
-    } else {
-        event.target.style.backgroundColor = 'red'
-    }
+    resetButton.addEventListener('click', () => {
 
+        const container = document.querySelector('#container')
+        container.remove()
+        const newBoard = new Board()
+
+        board.grid = newBoard.grid
+        createGridContainer()
+        console.log(board.grid)
+
+    })
 }
 
+window.addEventListener("DOMContentLoaded", () => {
 
-window.addEventListener('DOMContentLoaded', () => {
-
+    createResetButton();
     createGridContainer();
-})
+
+});
